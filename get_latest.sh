347bf919fcf1afe3cfae9647bb086c4c44d52c2b
Ed Guild <ed@vizzie.com>
2018-07-19 15:17:01 -0400

    initial stab at downloading latest version

---
+++ get_latest.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+release_ver=`wget -O- http://circuithappy.com/updates/missing-link/current.txt`
+wget -O /tmp/missing-link-update.zip http://circuithappy.com/updates/missing-link/missing-link-update-+release_ver+.zip
+unzip /tmp/missing-link-update.zip
