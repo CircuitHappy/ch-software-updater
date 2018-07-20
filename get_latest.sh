44c39d69a0c0c190173bf87d68e21b5ebd9451fc
Ed Guild <ed@vizzie.com>
2018-07-20 15:18:28 -0400

    Update get_latest.sh

--- get_latest.sh
+++ get_latest.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-release_ver=`curl http://circuithappy.com/updates/missing-link/current.txt`
+release_ver=`curl http://circuithappy.com/updates/missing-link/current.txt?`
 echo "release version:" $release_ver
 wget -O /tmp/missing-link-update.zip http://circuithappy.com/updates/missing-link/missing-link-update-$release_ver.zip
 unzip /tmp/missing-link-update.zip
