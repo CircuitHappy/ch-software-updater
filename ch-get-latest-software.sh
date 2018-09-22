48f8ef2aafc7331a9a00a76087542d0653f68c69
Ed Guild <ed@vizzie.com>
2018-09-22 14:50:15 +0000

    caught up changes to what was really on master

--- ch-get-latest-software.sh
+++ ch-get-latest-software.sh
@@ -3,7 +3,7 @@
 #set -x
 
 warn () {
-    echo "$0:" "$@" >&2
+    /bin/echo "$0:" "$@" >&2
 }
 die () {
     rc=$1
@@ -20,16 +20,16 @@ release_server_root="http://circuithappy.com/updates/missing-link"
 if [ ! -z "$1" ]
 then
   release_server_root="$release_server_root/$1"
-  echo "release_server_root is now:" $release_server_root
+  /bin/echo "release_server_root is now:" $release_server_root
 fi
 
 current_txt_url=$release_server_root/current.txt
 
 # check if current.txt exists, then wget to pick up the current release version
-if `wget -q $current_txt_url -O /dev/null`
+if `/usr/bin/wget -q $current_txt_url -O /dev/null`
 then
-  release_ver=`wget -O- $current_txt_url`
-  echo "release version:" $release_ver
+  release_ver=`/usr/bin/wget -O- $current_txt_url`
+  /bin/echo "release version:" $release_ver
 else
   die 2 "Could not find a release version file here: $current_txt_url, bailing."
 fi
@@ -40,7 +40,7 @@ then
 fi
 
 current_ln_path=`readlink -f /ch/current`
-echo "/ch/current links to:" $current_ln_path
+/bin/echo "/ch/current links to:" $current_ln_path
 
 # is the current release version already installed?
 if [ -d $CH_ROOT/${release_ver} ]
@@ -60,13 +60,13 @@ fi
 # if the release directory doesn't exist, then download and install
 if [ $release_directory_exists = false ]
 then
-  wget -O /tmp/missing-link-update-$release_ver.zip $release_server_root/missing-link-update-$release_ver.zip
+  /usr/bin/wget -O /tmp/missing-link-update-$release_ver.zip $release_server_root/missing-link-update-$release_ver.zip
   if [ $? != 0 ];
   then
      die 2 "wget returned a non 0 exit code trying to download missing-link-update-$release_ver.zip, bailing."
   fi
 
-  wget -O /tmp/missing-link-update-$release_ver.zip.md5 $release_server_root/missing-link-update-$release_ver.zip.md5
+  /usr/bin/wget -O /tmp/missing-link-update-$release_ver.zip.md5 $release_server_root/missing-link-update-$release_ver.zip.md5
   if [ $? != 0 ];
   then
      die 2 "wget returned a non 0 exit code trying to download missing-link-update-$release_ver.zip.md5, bailing."
@@ -78,21 +78,21 @@ then
   #6749bb24f706bbd4141cba5ba1081e72  foozle.zip
   #
 
-  expected_md5sum=`cat /tmp/missing-link-update-$release_ver.zip.md5 | awk '{print $1}'`
+  expected_md5sum=`/bin/cat /tmp/missing-link-update-$release_ver.zip.md5 | /usr/bin/awk '{print $1}'`
 
   if [ "x${expected_md5sum}" = "x" ];
   then
       die 3 "the md5sum from the server was missing or corrupt, bailing."
   fi
 
-  actual_md5sum=`md5sum /tmp/missing-link-update-$release_ver.zip | awk '{print $1}'`
+  actual_md5sum=`/usr/bin/md5sum /tmp/missing-link-update-$release_ver.zip | /usr/bin/awk '{print $1}'`
 
   if [ ${expected_md5sum} != ${actual_md5sum} ];
   then
      die 3 "the md5sum on the zip didn't match expected, bailing."
   fi
 
-  unzip /tmp/missing-link-update-$release_ver.zip -d $CH_ROOT/ > /tmp/unzip.out 2>&1
+  /usr/bin/unzip /tmp/missing-link-update-$release_ver.zip -d $CH_ROOT/ > /tmp/unzip.out 2>&1
   if [ $? != 0 ];
   then
      die 4 "unzip returned a non 0 exit code trying to extract the update (check /tmp/unzip.out), bailing."
@@ -100,24 +100,30 @@ then
 else
   # if "current" is pointing at another directory but current release is already installed
   # then we just need to relink the symlink to the current release directory
-  echo "Release directory already exists. Relinking that:" $CH_ROOT/${release_ver}
+  /bin/echo "Release directory already exists. Relinking that:" $CH_ROOT/${release_ver}
 fi
 
-rm $CH_ROOT/current
+/bin/rm $CH_ROOT/current
 if [ $? != 0 ] && [ -L $CH_ROOT/current ];
 then
     warn "this is bad, we couldn't remove the old current symlink "
 fi
 
 
-ln -s $CH_ROOT/$release_ver $CH_ROOT/current
+/bin/ln -s $CH_ROOT/$release_ver $CH_ROOT/current
 if [ $? != 0 ] && [ ! -L $CH_ROOT/current ];
 then
     die 5 "unable to relink things, your system may be unusable after a reboot."
 fi
 
-echo $release_ver > $CH_ROOT/version.txt
+/bin/echo $release_ver > $CH_ROOT/version.txt
 if [ $? != 0 ];
 then
    die 6 "Could not update version number file."
+fi
+
+if [ -f $CH_ROOT/${release_ver}/post.sh ];
+then
+   /bin/echo "post.sh script found. running it."
+   $CH_ROOT/${release_ver}/post.sh
 fi
