1c420214e9cf0fa10faf61060d481664198cd502
Ed Guild <ed@vizzie.com>
2018-09-27 19:12:07 +0000

    used wrong comparison operator

--- ch-get-latest-software.sh
+++ ch-get-latest-software.sh
@@ -16,8 +16,8 @@ CH_ROOT="/ch"
 
 release_server_root="http://circuithappy.com/updates/missing-link"
 
-software_update_status="NOT_STARTED"
-system_update_status="NOT_STARTED"
+software_update_status=0
+system_update_status=0
 
 # if we are passing in a beta download code, then we need to add that code to the server path
 if [ ! -z "$1" ]
@@ -29,7 +29,7 @@ fi
 /bin/sh $CH_ROOT/system/current/scripts/ch-update-missing-link.sh $release_server_root
 software_update_status=$?
 
-if $software_update_status > 1
+if $software_update_status -gt 1
 then
   die $software_update_status
 fi
@@ -37,13 +37,13 @@ fi
 /bin/sh $CH_ROOT/system/current/scripts/ch-update-system.sh $release_server_root
 system_update_status=$?
 
-if $system_update_status > 1
+if $system_update_status -gt 1
 then
   die $system_update_status
 fi
 
 #if both exit with a code of 1 that means there's no updates and no need to reboot
-if [[ $system_update_status = 1 && $software_update_status = 1 ]]
+if [[ $system_update_status -eq 1 && $software_update_status -eq 1 ]]
 then
   die 1
 fi
