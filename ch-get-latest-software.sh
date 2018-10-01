c191139c5c21d6487aa498710d4896c5dc3147ce
Ed Guild <ed@vizzie.com>
2018-10-01 19:16:41 +0000

    write beta_code to file and check for staging file, add -staging to the update URL

--- ch-get-latest-software.sh
+++ ch-get-latest-software.sh
@@ -14,14 +14,22 @@ die () {
 
 CH_ROOT="/ch"
 
+beta_code=""
+
 release_server_root="http://circuithappy.com/updates/missing-link"
 
 software_update_status=0
 system_update_status=0
 
+if [ -f "$CH_ROOT/staging" ]
+then
+  release_server_root="$release_server_root-staging"
+fi
+
 # if we are passing in a beta download code, then we need to add that code to the server path
 if [ ! -z "$1" ]
 then
+  beta_code="$1"
   release_server_root="$release_server_root/$1"
   /bin/echo "release_server_root is now:" $release_server_root
 fi
@@ -46,4 +54,11 @@ fi
 if [ $system_update_status -eq 1 ] && [ $software_update_status -eq 1 ]
 then
   die 1
+fi
+
+#update the beta_code to the beta_code.txt, it will contain the beta_code or be blank, if it's a mainline build
+/bin/echo "$beta_code" > $CH_ROOT/beta_code.txt
+if [ $? != 0 ];
+then
+   die 6 "Could not write $CH_ROOT/beta_code.txt"
 fi
